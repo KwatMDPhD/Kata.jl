43923228bfbe740c68d223dc0a958e454ab8d03a
KwatME <kwatme8@gmail.com>
2018-12-01 17:49:59 -0800

    Git sync (0)

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# black_ipynb
-Format `.ipynb` with black ::
+# Clean .ipynb
+
+Clean .ipynb (isort, black, clear output, and more) :sunflower:
+
+Install
+
+```sh
+pip install clean_ipynb
+```
+
+Example
+
+Clean `notebook/input.ipynb`
+
+```sh
+clean_ipynb notebook/input.ipynb
+```
+
+Find and clean ``\*.ipynb\`
+
+```sh
+find . -name '*.ipynb' -exec clean_ipynb {} \;
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+name = "clean_ipynb"
+
+setup(
+    name=name,
+    version="0.0.1",
+    url="https://github.com/KwatME/clean_ipynb",
+    author="Kwat ME",
+    author_email="kwatme8@gmail.com",
+    python_requires=">=3.6",
+    install_requires=("black", "click"),
+    packages=(name,),
+    py_modules=(name,),
+    entry_points={
+        "console_scripts": ("{0}={0}.{1}:{1}".format(name, "command_line_interface"),)
+    },
+)
