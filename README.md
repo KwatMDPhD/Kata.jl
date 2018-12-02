f0c6e47ab90c0c092c2e93769e448f99bef78984
KwatME <kwatme8@gmail.com>
2018-12-01 21:59:08 -0800

    Settled

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # Clean .ipynb
 
-Clean .ipynb ([isort](https://github.com/timothycrosley/isort), [black](https://github.com/ambv/black), clear output, and more) :sunflower:
+Clean .ipynb one cell at a time inplace by: running [isort](https://github.com/timothycrosley/isort), running [black](https://github.com/ambv/black), and clearing output :sunflower:
 
-# Install
+`clean_ipynb` is in beta, so use `--back-up` flag just in case.
+
+## Install
 
 ```sh
 pip install clean_ipynb
 ```
 
-# Example
+## Use
 
 Clean `notebook/test.ipynb`
 
 ```sh
-clean_ipynb notebook/input.ipynb
+clean_ipynb --back-up notebook/input.ipynb
 ```
 
 Find and clean `*.ipynb` recursively
 
 ```sh
-find . -name '*.ipynb' -exec clean_ipynb {} \;
+find . -name '*.ipynb' -exec clean_ipynb --back-up {} \;
 ```
--- clean_ipynb/command_line_interface.py
+++ clean_ipynb/command_line_interface.py
@@ -5,12 +5,17 @@ from .clean_ipynb import clean_ipynb
 
 @command()
 @argument("ipynb-file-paths", nargs=-1, type=Path(exists=True))
-@option("--back-up", "-b", is_flag=True, help="Whether to back up")
-@option("--clear-output", "-c", is_flag=True, help="Whether to clear output")
-def command_line_interface(ipynb_file_paths, back_up, clear_output):
+@option("--back-up", "-b", is_flag=True, help="Whether to back up .ipynb")
+@option("--keep-output", "-k", is_flag=True, help="Whether to keep output .ipynb")
+def command_line_interface(ipynb_file_paths, back_up, keep_output):
+    """
+    Clean .ipynb one cell at a time inplace by: running isort, running black, and clearing output.
+
+    clean_ipynb is in beta, so use --back-up flag just in case.
+    """
 
     for ipynb_file_path in ipynb_file_paths:
 
         secho(ipynb_file_path, bg="black", fg="bright_green")
 
-        clean_ipynb(ipynb_file_path, back_up=back_up, clear_output=clear_output)
+        clean_ipynb(ipynb_file_path, back_up, keep_output)
