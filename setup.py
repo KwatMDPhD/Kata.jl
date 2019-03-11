2c7f6e5a202473b4f16208a35a90d2bc0975a364
KwatME <kwatme8@gmail.com>
2019-03-10 21:15:03 -0700

    Surviving isort issue

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="kwatme8@gmail.com",
     packages=find_packages(),
     python_requires=">=3.6",
-    install_requires=("black", "click==7.0", "isort", "jupyter"),
+    install_requires=("black", "click==7.0", "isort==4.3.4", "jupyter"),
     entry_points={
         "console_scripts": ("{0}={0}.{1}:{1}".format(NAME, "command_line_interface"),)
     },
