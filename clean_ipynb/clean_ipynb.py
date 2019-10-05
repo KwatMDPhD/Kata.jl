3eb05f7fd3635aef0a43ee0431a403aa64b00d60
KwatME <kwatme8@gmail.com>
2019-10-04 23:56:42 -0700

    Merging

--- clean_ipynb/clean_ipynb.py
+++ clean_ipynb/clean_ipynb.py
@@ -7,21 +7,7 @@ from .has_julia_and_juliaformatter import has_julia_and_juliaformatter
 
 
 def clean_ipynb(ipynb_file_path, overwrite):
-    """Clean a Jupyter notebook.
 
-    Cleaning Python notebooks is supported out of the box, but Julia notebooks require
-    installation of Julia and JuliaFormatter.jl.
-
-    Args:
-        ipynb_file_path (str): Path to the notebook.
-        overwrite (bool): True to overwrite the existing notebook, False to write the
-            formatted output to file [orig_name].ipynb -> [orig_name].cleaned.ipynb.
-
-    Returns: None if successful, 1 if formatting could not be carried out. This could
-    be the case if prerequisites for Julia formatting have not been met, but a Julia
-    notebook is given.
-
-    """
     if not overwrite:
 
         ipynb_file_path = copyfile(
@@ -45,15 +31,8 @@ def clean_ipynb(ipynb_file_path, overwrite):
         clean_code = clean_julia_code
 
     else:
-        # If nothing can be cleaned, output the original file and return with code 1.
 
-        with open(ipynb_file_path, mode="w") as io:
-
-            dump(ipynb_dict, io, indent=1)
-
-            io.write("\n")
-
-        return 1
+        return
 
     cells = []
 
