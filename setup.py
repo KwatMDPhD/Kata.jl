7f01874b621e2caffa55ec8d0eaed18d03e2b06e
KwatME <kwatme8@gmail.com>
2020-03-25 16:01:15 -0700

    Synchronized to get_geo

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ name = "clean_ipynb"
 
 setup(
     name=name,
-    url="https://github.com/KwatME/{}".format(name),
     version="1.1.1",
-    author="Kwat Medetgul-Ernar",
-    author_email="kwatme8@gmail.com",
     python_requires=">=3.7",
     install_requires=("autoflake", "black", "click", "isort"),
     packages=(name,),
