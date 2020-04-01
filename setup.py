60f761a17dc3ea15d4d9ede4b8359d2161fb6972
KwatME <kwatme8@gmail.com>
2020-04-01 12:59:04 -0700

    (python) 3.7 ==> 3.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ name = "clean_ipynb"
 setup(
     name=name,
     version="1.1.1",
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     install_requires=("autoflake", "black", "click", "isort"),
     packages=(name,),
     entry_points={"console_scripts": ("{0}={0}.{1}:{1}".format(name, "cli"),)},
