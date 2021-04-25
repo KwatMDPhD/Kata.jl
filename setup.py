a7a57ddd430ecbbd5c6a69a558579725ec3e604b
KwatME <kwat.me@icloud.com>
2021-04-24 21:26:39 -0700

    al

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
-name = "cleannb"
+n = "cleannb"
 
 setup(
-    name=name,
+    name=n,
     version="3.0.0",
     python_requires=">=3.6",
     install_requires=("black", "click", "isort"),
-    packages=(name,),
-    entry_points={
-        "console_scripts": ("{0}={1}.{2}:{2}".format(name.lower(), name, "cli"),)
-    },
+    packages=(n,),
+    entry_points={"console_scripts": ("{0}={1}.{2}:{2}".format(n.lower(), n, "cli"),)},
 )
