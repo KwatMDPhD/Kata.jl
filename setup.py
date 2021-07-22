7b060d760147716b8c8e524124cff1a7a40f87e4
KwatME <kwat.me@icloud.com>
2021-07-21 20:02:41 -0600

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 na = "clean_nb"
 
 setup(
     name=na,
-    version="4.0.0",
+    version="4.1.0",
     url="https://github.com/KwatME/clean_nb",
     python_requires=">=3.6.0",
     install_requires=["black", "click", "isort"],
-    packages=[na],
+    packages=find_packages(),
     entry_points={
         "console_scripts": ["{0}={0}.{1}:{1}".format(na, "cli")],
     },
