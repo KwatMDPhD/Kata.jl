70b8ad04934149219a8293244488c1ae6fbc7e84
KwatME <kwat.me@icloud.com>
2021-08-10 00:06:55 -0600

    4.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ na = "clean_nb"
 
 setup(
     name=na,
-    version="4.1.0",
+    version="4.1.1",
     url="https://github.com/KwatME/clean_nb",
     python_requires=">=3.6.0",
     install_requires=["black", "click", "isort"],
