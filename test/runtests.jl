c8096c63b7e7a9b490d95f1b2f8604ecbb3229aa
KwatMDPhD <kwat.me@icloud.com>
2022-02-08 22:34:42 -0700

    Housekeep

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,30 @@
+TE = joinpath(tempdir(), "Clean.test")
+
+if isdir(TE)
+
+    rm(TE; recursive = true)
+
+end
+
+mkdir(TE)
+
+#using Revise
+using BenchmarkTools
+
 using Clean
 
 jl = joinpath(@__DIR__, "runtests.jl")
 
 nb = joinpath(@__DIR__, "runtests.ipynb")
 
-;
-
-## clean_jl
-
 Clean.clean_jl(jl)
 
-## clean_nb
-
 Clean.clean_nb(nb)
-
-## clean
 
 Clean.clean(jl)
 
 Clean.clean(nb)
 
 Clean.clean(jl, nb)
+
+rm(TE; recursive = true)
