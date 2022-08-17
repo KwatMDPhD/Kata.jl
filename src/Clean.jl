9d248759e553df349752a9a7b094f817d2c93d1c
KwatMDPhD <kwat.me@icloud.com>
2022-08-17 13:24:18 -0600

    Switched from @__DIR__ to pwd()

--- src/Clean.jl
+++ src/Clean.jl
@@ -4,7 +4,69 @@ using Comonicon
 using JuliaFormatter
 using OnePiece
 
-include("clean_nb.jl")
+function _clean_nb(pa; ke_ar...)
+
+    println("Formatting ", pa)
+
+    nb = OnePiece.dict.read(pa)
+
+    me = nb["metadata"]
+
+    ju = me["language_info"]["name"] == "julia"
+
+    if ju
+
+        me["language_info"]["version"] = string(VERSION)
+
+        me["kernelspec"]["name"] = "julia-$(VERSION.major).$(VERSION.minor)"
+
+    else
+
+        println("Language is not julia.")
+
+    end
+
+    ce_ = []
+
+    for ce in nb["cells"]
+
+        if ce["cell_type"] == "code"
+
+            co = join(ce["source"])
+
+            if isempty(strip(co))
+
+                continue
+
+            end
+
+            ce["execution_count"] = nothing
+
+            ce["outputs"] = []
+
+            if ju
+
+                li_ = split(JuliaFormatter.format_text(co; ke_ar...), "\n")
+
+                ce["source"] = [string.(li_[1:(end - 1)], "\n"); li_[end]]
+
+            end
+
+            ce["metadata"] = Dict()
+
+        end
+
+        push!(ce_, ce)
+
+    end
+
+    nb["cells"] = ce_
+
+    nb = OnePiece.dict.sort_recursively!(nb)
+
+    OnePiece.dict.write(pa, nb, id = 1)
+
+end
 
 """
 Command-line program for cleaning `Julia` files (`.jl`) and `Jupyter notebook`s (`.ipynb`)
@@ -29,7 +91,7 @@ Command-line program for cleaning `Julia` files (`.jl`) and `Jupyter notebook`s 
 
         elseif ex == ".ipynb"
 
-            clean_nb(pa; ke_ar...)
+            _clean_nb(pa; ke_ar...)
 
         else
 
