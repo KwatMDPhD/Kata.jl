341b7b7b20ed21ca640bd7d195558bc82dde1cf7
KwatMDPhD <kwat.me@icloud.com>
2025-01-19 18:55:49 -0700

    Data.pr all.json. An.pr 1.jl. Kata.jl -size 10k.

--- TEMPLATE.pr/test/runtests.jl
+++ TEMPLATE.pr/test/runtests.jl
@@ -1,12 +1,14 @@
-using TEMPLATE
+using Data
 
-for jl in readdir(; join = true)
+for jl in readdir(@__DIR__)
 
-    if jl == @__FILE__
+    if jl == "runtests.jl" || startswith(jl, '_')
 
         continue
 
     end
+
+    jl = joinpath(@__DIR__, jl)
 
     @info "🎬 Running $jl"
 
